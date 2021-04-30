7406c102cee29b53b89e8386b657b25265a9f88e
Abiral <abiralbhattarai101@gmail.com>
2021-04-30 23:08:27 +0545

    fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,c,n,i;
+	i = 0;
+	a = 0;
+	b = 1;
+	c = 0;
+	printf("How many fibonacci terms do you want??");
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		printf("%d \t",a);
+		c = a+b;
+		a = b;
+		b = c;
+	};
+};
