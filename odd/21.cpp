86c7704a48af026f8520a71a49fa2860d5241d15
Abiral <abiralbhattarai101@gmail.com>
2021-05-06 22:35:45 +0545

    qn21

---
+++ odd/21.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main(){
+	float x,y;
+	printf("enter the value of x: ");
+	scanf("%f",&x);
+	x<=2?y=2.4*x+3:y=3*x-5;
+	printf("%f",y);
+}
